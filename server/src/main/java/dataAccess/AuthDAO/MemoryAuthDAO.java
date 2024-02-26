8d4cdd83378401747ced1676bae812dbee78cae9
Logan McDonald <lm694@byu.edu>
2024-02-26 15:18:25 -0700

    Drafted MemoryAuthDAO and MemoryGameDAO

--- server/src/main/java/dataAccess/AuthDAO/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO/MemoryAuthDAO.java
@@ -1,4 +1,32 @@
 package dataAccess.AuthDAO;
+import dataAccess.AuthDAO.SQLAuthDAO;
+import model.AuthData;
 
-public class MemoryAuthDAO {
+import java.util.HashMap;
+
+public class MemoryAuthDAO implements SQLAuthDAO{
+  private final HashMap<String, AuthData> authentication = new HashMap<>();
+
+  @Override
+  public String createAuth(String username) {
+    String authToken = System.currentTimeMillis() + "-" + (int) (Math.random() * 1000);
+    AuthData auth = new AuthData(authToken, username);
+    authentication.put(authToken, auth);
+    return authToken;
+  }
+
+  @Override
+  public AuthData getAuth(String authToken) {
+    return authentication.get(authToken);
+  }
+
+  @Override
+  public void deleteAuth(String authToken) {
+    authentication.remove(authToken);
+  }
+
+  @Override
+  public void deleteAuthTotal() {
+    authentication.clear();
+  }
 }
--- server/src/main/java/dataAccess/GameDAO/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/GameDAO/MemoryGameDAO.java
@@ -1,4 +1,42 @@
 package dataAccess.GameDAO;
 
-public class MemoryGameDAO {
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class MemoryGameDAO implements SQLGameDAO{
+  private final HashMap<Integer, GameData> games = new HashMap<>();
+  private AtomicInteger gameIdGenerator = new AtomicInteger(0);
+
+
+  @Override
+  public Integer createGame(String gameName) {
+    Integer gameID = (int) (System.currentTimeMillis() % 1000000) * 1000 + gameIdGenerator.incrementAndGet();
+    games.put(gameID, new GameData(gameID, null, null, gameName, new ChessGame()));
+    return gameID;
+  }
+
+  @Override
+  public GameData getGame(int gameID) {
+    return games.get(gameID);
+  }
+
+  @Override
+  public ArrayList<GameData> listGames() {
+    return (ArrayList<GameData>) games.values();
+  }
+
+  @Override
+  public void updateGame(GameData game) {
+    games.put(game.gameID(), game);
+  }
+
+  @Override
+  public void deleteGames() {
+    games.clear();
+  }
 }
