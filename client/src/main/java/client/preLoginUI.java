da10c963264377f526093ba3a6f31f42bc5ddd00
Logan McDonald <lm694@byu.edu>
2024-03-20 14:14:59 -0600

    Added most necessary files

---
+++ client/src/main/java/client/preLoginUI.java
@@ -0,0 +1,11 @@
+package client;
+import model.login.LoginResult;
+import model.register.RegisterResult;
+import client.ResponseException;
+import client.ServerFacade;
+
+import java.util.Arrays;
+
+public class preLoginUI {
+
+}
