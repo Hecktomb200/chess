e4dd9805f31da44b98829855eec8b197bc174cec
Logan McDonald <lm694@byu.edu>
2024-03-20 14:17:44 -0600

    Added Exception class

--- client/src/main/java/client/ResponseException.java
+++ client/src/main/java/client/ResponseException.java
@@ -1,4 +1,14 @@
 package client;
 
-public class ResponseException {
+public class ResponseException extends Exception {
+    private int statCode;
+
+    public ResponseException(int statCode, String mes) {
+        super(mes);
+        this.statCode = statCode;
+    }
+
+    public int StatusCode() {
+        return statCode;
+    }
 }
