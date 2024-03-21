08fc793526ee53ce1056f15f0c4775c4d315ebe3
Logan McDonald <lm694@byu.edu>
2024-03-21 15:19:06 -0600

    Drafted run(),help(),and createGame() in PostLoginUI.java

--- client/src/main/java/client/PostLoginUI.java
+++ client/src/main/java/client/PostLoginUI.java
@@ -7,6 +7,10 @@ import client.ServerFacade;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class PostLoginUI {
     private final ServerFacade server;
     private final HashMap<Integer, GameData> gameList;
@@ -25,12 +29,56 @@ public class PostLoginUI {
             var params = Arrays.copyOfRange(integers, 1, integers.length);
             return switch (prompt) {
                 case "list" -> listGames();
-                //case "create" -> createGame(params);
-                //default -> help();
+                case "create" -> createGame(params);
+                default -> help();
             };
         } catch (ResponseException ex) {
             return ex.getMessage();
         }
+    }
+
+    public void run(String username) {
+        System.out.println("Logged in as " + username + ".");
+        System.out.print(this.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("logout")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = this.getCommands(line);
+                System.out.print(SET_TEXT_COLOR_YELLOW + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        try {
+            System.out.println();
+            this.logout();
+        } catch (Throwable e) {
+            var msg = e.toString();
+            System.out.print(msg);
+        }
+
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + SET_TEXT_COLOR_GREEN + "[LOGGED IN]>>> " + SET_TEXT_COLOR_BLUE);
+    }
+
+    public String help() {
+        return """
+                    - create <GAME NAME>
+                    - list
+                    - join <ID> [WHITE | BLACK]
+                    - observe <ID>
+                    - logout
+                    - quit
+                    - help
+                    """;
     }
 
     public String listGames() throws ResponseException {
@@ -45,5 +93,13 @@ public class PostLoginUI {
             gameList.put(i, game);
         }
         return list;
+    }
+
+    public String createGame(String... params) throws ResponseException {
+        if (params.length == 1) {
+            server.createGame(params[0], authToken);
+            return String.format("Chess game %s created.", params[0]);
+        }
+        throw new ResponseException(400, "Expected: <GAME NAME>");
     }
 }
