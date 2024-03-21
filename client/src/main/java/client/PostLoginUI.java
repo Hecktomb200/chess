6bebca2c6b11beaf1cba03b6c9d59b00008a94ea
Logan McDonald <lm694@byu.edu>
2024-03-21 14:53:05 -0600

    Drafted getCommands() and listGames() in PostLoginUI.java

--- client/src/main/java/client/PostLoginUI.java
+++ client/src/main/java/client/PostLoginUI.java
@@ -1,4 +1,49 @@
 package client;
 
+import model.GameData;
+import model.listGames.ListGamesResult;
+import client.ResponseException;
+import client.ServerFacade;
+
+import java.util.Arrays;
+import java.util.HashMap;
 public class PostLoginUI {
+    private final ServerFacade server;
+    private final HashMap<Integer, GameData> gameList;
+    private final String authToken;
+
+    public PostLoginUI(String auth, String url) {
+        server = new ServerFacade(url);
+        gameList = new HashMap<>();
+        authToken = auth;
+    }
+
+    public String getCommands(String input) {
+        try {
+            var integers = input.toLowerCase().split(" ");
+            var prompt = (integers.length > 0) ? integers[0] : "help";
+            var params = Arrays.copyOfRange(integers, 1, integers.length);
+            return switch (prompt) {
+                case "list" -> listGames();
+                //case "create" -> createGame(params);
+                //default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String listGames() throws ResponseException {
+        ListGamesResult gamesListed = server.listGames(authToken);
+        String list = "";
+        int i = 0;
+        var games = gamesListed.games();
+
+        for(GameData game: games) {
+            list += i + ": " + game.gameName() + "\n";
+            i++;
+            gameList.put(i, game);
+        }
+        return list;
+    }
 }
