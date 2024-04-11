5b260a30347cf63374db9ab390ee5e9b1926da3a
Logan McDonald <lm694@byu.edu>
2024-04-11 14:30:05 -0600

    Drafted ResignCommand.java

--- shared/src/main/java/webSocketMessages/userCommands/ResignCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/ResignCommand.java
@@ -1,4 +1,13 @@
 package webSocketMessages.userCommands;
 
-public class ResignCommand {
+public class ResignCommand extends UserGameCommand{
+    private Integer gameID;
+
+    public ResignCommand(String authToken, Integer gameIdentifier) {
+        super(authToken);
+        gameID = gameIdentifier;
+        this.commandType = CommandType.RESIGN;
+    }
+
+    public Integer getGameID() { return gameID; }
 }
