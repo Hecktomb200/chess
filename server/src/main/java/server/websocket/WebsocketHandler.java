8dd8c800f0e604bd76cfdcfbda161e00c1ffbc09
Logan McDonald <lm694@byu.edu>
2024-04-10 20:32:10 -0600

    Drafted joinPlayer

--- server/src/main/java/server/websocket/WebsocketHandler.java
+++ server/src/main/java/server/websocket/WebsocketHandler.java
@@ -6,12 +6,13 @@ import dataAccess.DataAccessException;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import service.GamesService;
-//import webSocketMessages.serverMessages.Error;
-//import webSocketMessages.serverMessages.LoadGame;
-//import webSocketMessages.serverMessages.Notification;
+import webSocketMessages.serverMessages.Error;
+import webSocketMessages.serverMessages.LoadGame;
+import webSocketMessages.serverMessages.Notification;
 import webSocketMessages.serverMessages.ServerMessage;
 import webSocketMessages.userCommands.*;
 
+import javax.management.Notification;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
@@ -55,9 +56,23 @@ public class WebsocketHandler {
         
     }
 
-    private void joinPlayer(Object fromJson, Session session) {
-        
+    private void joinPlayer(JoinPlayer player, Session session) throws IOException, DataAccessException {
+        String resp = gamesService.joinPlayer(player);
+
+        if (resp.contains("Error")) {
+            sendMessage(new Error(resp), session);
+            return;
+        }
+        session.addSessionToGame(player.getGameID(), player.getAuthString(), session);
+        sendMessage(new LoadGame(player.getGameID()), session);
+        String color;
+        if(player.getPlayerColor() == ChessGame.TeamColor.WHITE) {
+            color = "white";
+        } else {
+            color = "black";
+        }
+        var message = String.format("%s is joining as %s", player.getPlayerName(),color);
+        var notification = new Notification(message);
+        broadcastMessage(player.getGameID(), notification, player.getAuthString());
     }
-
-
 }
