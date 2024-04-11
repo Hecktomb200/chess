a334a21c4db13e188898dc2fe16b955575c7e9b3
Logan McDonald <lm694@byu.edu>
2024-04-11 14:12:43 -0600

    Drafted LeaveCommand.java

--- shared/src/main/java/webSocketMessages/userCommands/LeaveCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/LeaveCommand.java
@@ -1,4 +1,13 @@
 package webSocketMessages.userCommands;
 
-public class LeaveCommand {
+public class LeaveCommand extends UserGameCommand{
+    private final Integer gameID;
+
+    public LeaveCommand(String authToken, Integer gI) {
+        super(authToken);
+        gameID = gI;
+        this.commandType = CommandType.LEAVE;
+    }
+
+    public Integer getGameID() { return gameID; }
 }
