4b4ef628eceaf7531d6c8e77570cab4a3c562e59
Logan McDonald <lm694@byu.edu>
2024-10-22 18:12:18 -0600

    GameService.java- Built createGame().

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,7 +1,11 @@
 package service;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
+import model.AuthData;
+import model.createGame.CreateGameRequest;
+import model.createGame.CreateGameResult;
 
 public class GameService {
   private AuthDAO authDAO;
@@ -10,6 +14,24 @@ public class GameService {
   public GameService (AuthDAO authDAO, GameDAO gameDAO) {
     this.authDAO = authDAO;
     this.gameDAO = gameDAO;
+  }
+
+  public CreateGameResult createGame(CreateGameRequest gameRequest, String authToken) throws DataAccessException {
+    AuthData auth = authDAO.getAuth(authToken);
+
+    if (auth == null) {
+      throw new DataAccessException("Unauthorized");
+    }
+    verifyGame(gameRequest.gameName());
+
+    int gameID = gameDAO.createGame(gameRequest.gameName());
+    return new CreateGameResult(gameID);
+  }
+
+  public void verifyGame(String gameName) throws DataAccessException {
+    if (gameName == null || gameName.isEmpty()) {
+      throw new DataAccessException("Not found");
+    }
   }
 
 }
