52d733d35d385fb58f6c500ff236b980494d9c83
Logan McDonald <lm694@byu.edu>
2024-10-31 00:46:40 -0600

    SQLAuthDAO.java- Built getAuth. Definitely confused.

--- server/src/main/java/dataaccess/database/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/database/SQLAuthDAO.java
@@ -26,4 +26,23 @@ public class SQLAuthDAO {
       throw new DataAccessException("Failed to create auth: " + e.getMessage());
     }
   }
+
+  public AuthData getAuth(String authToken) throws DataAccessException{
+    try (var connection = DatabaseManager.getConnection()) {
+      var statement = "SELECT authToken, username FROM auth WHERE authToken=?";
+      try (var preparedStatement = connection.prepareStatement(statement)) {
+        preparedStatement.setString(1, authToken);
+        try (var resultSet = preparedStatement.executeQuery()) {
+          if (resultSet.next()) {
+            var newToken = resultSet.getString(1);
+            var username = resultSet.getString(2);
+            return new AuthData(newToken, username);
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new DataAccessException(e.toString());
+    }
+    return null;
+  }
 }
