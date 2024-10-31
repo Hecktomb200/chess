deafd21a1cf09fbfd1401494f52619f17ec56cee
Logan McDonald <lm694@byu.edu>
2024-10-30 22:48:29 -0600

    SQLAuthDAO.java- Built createAuth. I might be confused.

--- server/src/main/java/dataaccess/database/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/database/SQLAuthDAO.java
@@ -1,4 +1,29 @@
 package dataaccess.database;
 
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import model.AuthData;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class SQLAuthDAO {
+
+
+
+  public AuthData createAuth(String username) throws DataAccessException {
+    String authToken = UUID.randomUUID().toString();
+    String insertSQL = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+
+    try (var connection = DatabaseManager.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+      preparedStatement.setString(1, authToken);
+      preparedStatement.setString(2, username);
+      preparedStatement.executeUpdate();
+      return new AuthData(authToken, username);
+    } catch (SQLException e) {
+      throw new DataAccessException("Failed to create auth: " + e.getMessage());
+    }
+  }
 }
