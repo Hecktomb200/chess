4d3ed72be995a39b1c0fb1d7a95d46a1d8db5c79
Logan McDonald <lm694@byu.edu>
2024-11-01 15:03:36 -0600

    SQLUserDAO.java- Added some error handliing.

--- server/src/main/java/dataaccess/database/SQLUserDAO.java
+++ server/src/main/java/dataaccess/database/SQLUserDAO.java
@@ -20,22 +20,28 @@ public class SQLUserDAO extends UserDAO {
   }
 
   @Override
-  public void createUser(String username, String password, String email) throws DataAccessException {
-    String insertStatement="INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+  public void createUser (String username, String password, String email) throws DataAccessException {
+    if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
+      throw new DataAccessException("Error: username or password cannot be empty");
+    }
+    String insertStatement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
     executeUpdate(insertStatement, username, password, email);
   }
 
   @Override
-  public UserData getUser(String username) throws DataAccessException {
-    String selectStatement="SELECT username, password, email FROM user WHERE username = ?";
-    try (Connection connection=DatabaseManager.getConnection();
-         PreparedStatement preparedStatement=connection.prepareStatement(selectStatement)) {
+  public UserData getUser (String username) throws DataAccessException {
+    if (username == null || username.isEmpty()) {
+      throw new DataAccessException("Error: bad request");
+    }
+    String selectStatement = "SELECT username, password, email FROM user WHERE username = ?";
 
+    try (Connection connection = DatabaseManager.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(selectStatement)) {
       preparedStatement.setString(1, username);
-      try (ResultSet resultSet=preparedStatement.executeQuery()) {
+      try (ResultSet resultSet = preparedStatement.executeQuery()) {
         if (resultSet.next()) {
-          String password=resultSet.getString("password");
-          String email=resultSet.getString("email");
+          String password = resultSet.getString("password");
+          String email = resultSet.getString("email");
           return new UserData(username, password, email);
         }
       }
@@ -47,18 +53,17 @@ public class SQLUserDAO extends UserDAO {
 
   @Override
   public void clearUsers() throws DataAccessException {
-    String truncateStatement="TRUNCATE user";
+    String truncateStatement = "TRUNCATE user";
     executeUpdate(truncateStatement);
   }
 
   private int executeUpdate(String statement, Object... parameters) throws DataAccessException {
-    try (Connection connection=DatabaseManager.getConnection();
-         PreparedStatement preparedStatement=connection.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-
+    try (Connection connection = DatabaseManager.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
       setParameters(preparedStatement, parameters);
       preparedStatement.executeUpdate();
 
-      try (ResultSet resultSet=preparedStatement.getGeneratedKeys()) {
+      try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
         if (resultSet.next()) {
           return resultSet.getInt(1);
         }
@@ -70,8 +75,8 @@ public class SQLUserDAO extends UserDAO {
   }
 
   private void setParameters(PreparedStatement preparedStatement, Object... parameters) throws SQLException {
-    for (int i=0; i < parameters.length; i++) {
-      Object param=parameters[i];
+    for (int i = 0; i < parameters.length; i++) {
+      Object param = parameters[i];
       if (param instanceof String) {
         preparedStatement.setString(i + 1, (String) param);
       } else if (param == null) {
