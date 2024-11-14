e8891d4f5e15f931a7a52934321b1d36aa72bd13
Logan McDonald <lm694@byu.edu>
2024-11-14 13:06:45 -0700

    PostLoginUI.java- Built run.

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -1,10 +1,40 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class PostLoginUI {
   public PostLoginUI(String url, String authToken, String username) {
     
   }
 
   public void run(String username) {
+    System.out.println("Logged in as " + username + ".");
+    help();
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      String command = " ";
+      do {
+        System.out.print("\n" + SET_TEXT_COLOR_GREEN + "[LOGGED_IN] >>> " + SET_TEXT_COLOR_BLUE);
+        command = scanner.nextLine();
+        processCommand(command);
+        System.out.print(SET_TEXT_COLOR_YELLOW + command);
+      } while (!command.equals("logout"));
+      System.out.println();
+      logout();
+    } catch (Throwable e) {
+      System.out.print(e.toString());
+    }
+  }
+
+  private void processCommand(String command) {
+
+  }
+
+  private void help() {
+  }
+
+  private void logout() {
   }
 }
