31891e8ce89f85878bd6ad4d43283d73f830f711
Logan McDonald <lm694@byu.edu>
2024-11-14 14:06:50 -0700

    PostLoginUI.java- Finally figured out list.

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -1,14 +1,28 @@
 package ui;
 
+import model.GameData;
+import model.listgames.ListGamesResult;
+import server.ServerFacade;
+
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
 
 public class PostLoginUI {
+  private final ServerFacade server;
+  private final String sURL;
+  private final String authToken;
+  private final HashMap<Integer, GameData> gameList;
   public PostLoginUI(String url, String authToken, String username) {
-    
+    server = new ServerFacade(url);
+    sURL = url;
+    this.authToken = authToken;
+    gameList = new HashMap<>();
+
   }
 
   public void run(String username) {
@@ -30,7 +44,7 @@ public class PostLoginUI {
     }
   }
 
-  private void processCommand(String input) {
+  private void processCommand(String input) throws IOException, URISyntaxException {
     var integers=input.toLowerCase().split(" ");
     var command=(integers.length > 0) ? integers[0] : "help";
     var params=Arrays.copyOfRange(integers, 1, integers.length);
@@ -72,13 +86,46 @@ public class PostLoginUI {
     return false;
   }
 
-  private boolean list() {
-    return false;
+  private String list() throws IOException, URISyntaxException {
+    gameList.clear();
+    ListGamesResult gamesListed = getGames();
+    return buildGamesList(gamesListed);
+  }
+
+  private ListGamesResult getGames() throws IOException, URISyntaxException {
+    return server.listGames(authToken);
+  }
+
+  private String buildGamesList(ListGamesResult gamesListed) {
+    StringBuilder formattedList = new StringBuilder();
+    var games = gamesListed.games();
+    int index = 0;
+    for (GameData game : games) {
+      formattedList.append(formatGameEntry(index, game));
+      gameList.put(index, game);
+      index++;
+    }
+    return formattedList.toString();
+  }
+
+  private String formatGameEntry(int index, GameData game) {
+    return String.format("%d: %s -- White Player: %s, Black Player: %s%n",
+            index, game.gameName(), game.whiteUsername(), game.blackUsername());
   }
 
   private void help() {
+    System.out.println("""
+                - list
+                - create <GAME NAME>
+                - join <ID> [WHITE | BLACK]
+                - observe <ID>
+                - logout
+                - quit
+                - help
+                """);
   }
 
-  private void logout() {
+  private void logout() throws IOException, URISyntaxException {
+    server.logoutUser(authToken);
   }
 }
