778c74d8c690a93455ebeae23de66df791017c41
Logan McDonald <lm694@byu.edu>
2024-11-14 13:44:24 -0700

    PostLoginUI.java- Built processCommand.

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
@@ -28,8 +30,50 @@ public class PostLoginUI {
     }
   }
 
-  private void processCommand(String command) {
+  private void processCommand(String input) {
+    var integers=input.toLowerCase().split(" ");
+    var command=(integers.length > 0) ? integers[0] : "help";
+    var params=Arrays.copyOfRange(integers, 1, integers.length);
 
+    switch (command) {
+      case "list":
+        System.out.print(list());
+        break;
+      case "create":
+        System.out.print(create(params));
+        break;
+      case "join":
+        System.out.print(join(params));
+        break;
+      case "observe":
+        System.out.print(observe(params));
+        break;
+      case "logout":
+        System.out.println("logout");
+        break;
+      case "quit":
+        System.out.println("quit");
+        break;
+      default:
+        help();
+        break;
+    }
+  }
+
+  private String observe(String[] params) {
+    return null;
+  }
+
+  private String join(String[] params) {
+    return null;
+  }
+
+  private boolean create(String[] params) {
+    return false;
+  }
+
+  private boolean list() {
+    return false;
   }
 
   private void help() {
