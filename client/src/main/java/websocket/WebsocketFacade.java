b107b1132e9772b2ce442f35a679b0fb4b55a559
Logan McDonald <lm694@byu.edu>
2024-12-05 02:33:22 -0700

    WebsocketFacade.java- Built accompanying methods for handleServerMessage.

--- client/src/main/java/websocket/WebsocketFacade.java
+++ client/src/main/java/websocket/WebsocketFacade.java
@@ -1,14 +1,19 @@
 package websocket;
 
+import websocket.messages.LoadMessage;
+import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
+import websocket.messages.Error;
 
 import javax.websocket.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.logging.Logger;
 
 public class WebsocketFacade extends Endpoint{
   private Session session;
+  private static final Logger logger = Logger.getLogger(WebsocketFacade.class.getName());
 
   public WebsocketFacade(String url) throws IOException {
     try {
@@ -21,9 +26,7 @@ public class WebsocketFacade extends Endpoint{
   }
 
   @Override
-  public void onOpen(Session session, EndpointConfig endpointConfig) {
-
-  }
+  public void onOpen(Session session, EndpointConfig endpointConfig) {}
 
   @OnMessage
   public void onMessage(String message) {
@@ -34,7 +37,23 @@ public class WebsocketFacade extends Endpoint{
   }
 
   private void handleServerMessage(ServerMessage serverMessage) {
+    switch (serverMessage.getServerMessageType()) {
+      case LOAD_GAME -> handleLoadGame((LoadMessage) serverMessage);
+      case NOTIFICATION -> handleNotification(((NotificationMessage) serverMessage).getMessage());
+      case ERROR -> handleError(((Error)serverMessage).getErrorMessage());
+    }
+  }
 
+  private void handleLoadGame(LoadMessage loadMessage) {
+    logger.info("Game loaded: " + loadMessage);
+  }
+
+  private void handleNotification(String message) {
+    logger.info("Notification: " + message);
+  }
+
+  private void handleError(String errorMessage) {
+    logger.severe("Error: " + errorMessage);
   }
 
   private ServerMessage parseServerMessage(String message) {
